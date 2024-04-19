2dea7378bf715512091049e8ce978a4dadee05af
Avish K <49350400+AK1089@users.noreply.github.com>
2024-04-19 18:49:37 +0100

    fixing age

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
-// title alternator
+// on load
 document.addEventListener('DOMContentLoaded', function() {
+
+    // title updator
     var titleElement = document.getElementById('title');
     
     titleElement.addEventListener('click', function() {
@@ -9,4 +11,17 @@ document.addEventListener('DOMContentLoaded', function() {
             titleElement.textContent = 'Avish';
         }
     });
+
+    // age updator
+    var ageElement = document.getElementById('age');
+    var tooltipElement = document.getElementById('tooltip');
+    var birthDate = new Date('2004-02-07');
+    var currentDate = new Date();
+    var age = currentDate.getFullYear() - birthDate.getFullYear();
+    var monthDiff = currentDate.getMonth() - birthDate.getMonth();
+    if (monthDiff < 0 || (monthDiff === 0 && currentDate.getDate() < birthDate.getDate())) {
+        age--;
+    }
+    ageElement.textContent = age;
+    tooltipElement.textContent = 'Birthday: ' + birthDate.toDateString();
 });
