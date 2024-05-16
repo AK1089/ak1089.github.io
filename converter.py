7423be76c7456ab7a99bdecf061128adeda04d1b
Avish K <49350400+AK1089@users.noreply.github.com>
2024-05-16 17:28:54 +0100

    tidying up

--- converter.py
+++ converter.py
@@ -17,7 +17,6 @@ def convert_markdown_to_html(input_file, output_file):
     # Ignore commented lines
     content = re.sub(r'\/\*.*\*\/', "", content, flags=re.MULTILINE)
 
-    
     # Replace line breaks with paragraph tags
     content = content.replace('\n\n', '</p>\n<p>')
     content = f'<p>{content}</p>\n'
@@ -33,11 +32,11 @@ def convert_markdown_to_html(input_file, output_file):
     content = re.sub(r'\[(.*)\]\(!(.+?)\)', r'<span class="tooltip">\1<span class="tooltiptext">\2</span></span>', content)
     content = re.sub(r'\[(.*)\]\((.*?)\)', r'<a href="\2">\1</a>', content)
 
-    # Replace color tags
+    # Color tags
     for color, hex_value in color_dict.items():
         content = re.sub(r'<&{}>(.*?)</&{}>'.format(color, color), r'<span style="color:{};">\1</span>'.format(hex_value), content)
 
-    # Replace info box syntax
+    # Info box
     def info_box_replacer(match):
         primary_color = match.group(1)
         secondary_color = match.group(2)
@@ -63,16 +62,18 @@ def convert_markdown_to_html(input_file, output_file):
     # Inline code
     content = re.sub(r'`([^`]+)`', r'<code class="inline-code">\1</code>', content)
 
-    # Lists
+    # List elements
     content = re.sub(r'^- (.+)$', r'<li>\1</li>', content, flags=re.MULTILINE)
 
-    # Spoiler
+    # Spoilers
     content = re.sub(r'\[spoiler\](.*?)\[\/spoiler\]', r'<span class="spoiler">\1</span>', content, flags=re.DOTALL)
 
+    # Add all content to the template file and write it to the output
     html_content = html_content.replace("ALL_CONTENT_GOES_HERE", content)
-
     with open(output_file, 'w') as f:
         f.write(html_content)
 
-# Example usage
-convert_markdown_to_html('example.txt', 'index.html')
+
+# Runs this for our standard file
+if __name__ == "__main__":
+    convert_markdown_to_html('index.txt', 'index.html')
