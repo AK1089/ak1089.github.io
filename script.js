e4af5436497f8017330ea71e391f35784b69a90c
Avish K <49350400+AK1089@users.noreply.github.com>
2024-05-16 16:28:40 +0100

    dynamically loading content

--- script.js
+++ script.js
@@ -11,3 +11,39 @@ function copyToClipboard(button) {
         button.textContent = 'Copy';
     }, 2000);
 }
+
+// Function to replace variable placeholders with their values
+function replaceVariables() {
+
+    var birthDate = new Date('2004-02-07');
+    var currentDate = new Date();
+    var age = currentDate.getFullYear() - birthDate.getFullYear();
+    var monthDiff = currentDate.getMonth() - birthDate.getMonth();
+    if (monthDiff < 0 || (monthDiff === 0 && currentDate.getDate() < birthDate.getDate())) {
+        age--;
+    }
+    
+    current_age_in_days = 'I was born on ' + birthDate.toDateString() + ', making me ' + (Math.floor((currentDate.getTime() - birthDate.getTime()) / (1000 * 60 * 60 * 24))).toString() + ' days old.';
+
+    // Define your global variable dictionary
+    const variables = {
+        variable_content: "Hello, World!",
+        current_age_in_years: age.toString(),
+        current_age_in_days: current_age_in_days.toString()
+    };
+    
+    // Get the entire HTML content
+    let html = document.body.innerHTML;
+
+    // Loop through the dictionary and replace each placeholder
+    for (const [key, value] of Object.entries(variables)) {
+        const variableRegex = new RegExp(`\\$\\{${key}\\}`, 'g');
+        html = html.replace(variableRegex, value);
+    }
+    
+    // Update the HTML content
+    document.body.innerHTML = html;
+}
+
+// Run the replaceVariables function on page load
+window.onload = replaceVariables;
